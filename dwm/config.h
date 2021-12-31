08ff4de5459cc038c5ee650918c3e84790bfc415
Nathan <nstefanik@gatech.edu>
2021-12-30 19:03:30 -0500

    Changed dmenu colors and added passmenu script

--- dwm/config.h
+++ dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 16;        /* gap size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14" };
-static const char dmenufont[]       = "monospace:size=14";
+static const char *fonts[]          = { "Fira Mono:size=12" };
+static const char dmenufont[]       = "Fira Mono:size=12";
 // background color
 static const char col_gray1[]       = "#282828";
 // inactive window border color
@@ -18,10 +18,19 @@ static const char col_gray3[]       = "#d5c4a1";
 static const char col_gray4[]       = "#fbf1c7";
 // top bar second color and active window border color
 static const char col_cyan[]        = "#d79921";
+/* gruvbox colors */
+static const char col_fg[]          = "#ebdbb2";
+static const char col_bg[]          = "#282828";
+static const char col_bg1[]         = "#3c3836";
+static const char col_gray[]        = "#a89984";
+static const char col_green[]       = "#98971a";
+static const char col_aqua[]        = "#8ec07c";
+static const char col_yellow[]      = "#d79921";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg, col_bg, col_gray2 },
+	[SchemeSel]  = { col_fg, col_yellow,  col_yellow  },
 };
 
 /* tagging */
@@ -63,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]     = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[]     = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_bg1, "-sf", col_yellow, NULL };
 static const char *termcmd[]      = { "kitty", NULL };
 static const char *browsercmd[]   = { "firefox", NULL };
 static const char *voldown[]	    = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL};
